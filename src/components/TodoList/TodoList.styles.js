1fd51f75d50b025983dbe8c0b167cb3a152246e8
Drekavac <diesel39931@gmail.com>
2020-06-09 12:57:16 +0600

    styled

---
+++ src/components/TodoList/TodoList.styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const BigRedText = styled.div``;
+
+const ListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  width: 50%;
+  margin: 0 auto;
+  padding-top: 20px;
+`;
---
+++ src/components/TodoListItem/TodoListItem.styles.js
@@ -0,0 +1,28 @@
+.todo-item-container {
+  width: 40%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.todo-item-title {
+  border: none;
+  border-top: 1px solid #222;
+  border-bottom: 1px solid #222;
+  width: 100%;
+  text-align: center;
+  padding: 10px 0 10px 0;
+}
+
+.buttons-container {
+  width: 100%;
+}
+
+.completed-button,
+.remove-button {
+  border-radius: 5px;
+  border: 1px solid #222;
+  background-color: transparent;
+  height: 40px;
+  width: 50%;
+}
